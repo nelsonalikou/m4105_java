6a47650d5135d4101c310d560049d98874485961
alik0002 <nelson.alikou-dongmo@etudiant.univ-reims.fr>
2021-01-15 18:35:53 +0100

    Fin du cours arret à la fonction estunPalindrome

--- src/bases/Principal.java
+++ src/bases/Principal.java
@@ -15,12 +15,14 @@ public class Principal {
 		String prenom = "Nelson";
 		int longueurPrenom = prenom.length();
 		System.out.print(prenom);
-		System.out.print("("+longueurPrenom+" caracteres)");
-		System.out.print("("+longueurPrenom+" caracteres)");
-		System.out.println("("+longueurPrenom+" caracteres)");
+		System.out.print("("+longueurPrenom + " caracteres)");
+		System.out.print("("+longueurPrenom + " caracteres)");
+		System.out.println("("+longueurPrenom + " caracteres)");
 
-		System.out.println("Le plus grand est"+maximum(5,15));
-		System.out.println("Le masque est "+ masquerLettres("toto","*"));
+		System.out.println("Le plus grand est "+maximum(5,15));
+		System.out.println("Le masque est "+ masquerLettres("toto",'*'));
+		System.out.println("le mot toto est il un palindrome ? "+ estUnPalindrome("ttto"));
+
 	}
 
 	/**
@@ -48,12 +50,43 @@ public class Principal {
 	 * @param masque caractère de remplissage
 	 * @return version cachée de la chaine
 	 */
-	public static String masquerLettres(String lettre,String masque) {
+	public static String masquerLettres(String lettre,char masque) {
 		String lettresCachees = "";
 		for(int i=0; i < lettre.length(); i++)
 		{
 			lettresCachees = lettresCachees + masque;
 		}
 		return lettresCachees;
+	}
+
+	/**
+	 * La chaine est-t-elle un palindrome cad a a des carctères symétriques par raport à son centre.
+	 * @param str
+	 * @return
+	 */
+	public static boolean estUnPalindrome(String str) {
+		boolean palindrome = true;
+		int taille = str.length();
+		int moitie = taille / 2;
+
+		int cpt = 0;
+		while (cpt < moitie && palindrome == true) {
+			if( str.charAt(cpt) != str.charAt(taille - cpt - 1) ){
+				System.out.println(str.charAt(cpt));
+				System.out.println(str.charAt(taille - cpt - 1));
+				palindrome = false;
+			}
+		}
+		return palindrome;
+	}
+
+	/** Index de la 1er occurence de la chaine recherchée dans la chaine source.
+     *
+     * @param chaineSource chaine source
+     * @param chaineRecherche chaine recherchée
+     * @return indice si trouvé, -1 sinon
+     */
+	public static int recherche(String chaineSource, String chaineRecherche) {
+		return 0;
 	}
 }
