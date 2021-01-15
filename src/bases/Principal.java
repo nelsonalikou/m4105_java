dca6ede53fa5d006a102f638d0e5cc6425b72468
alik0002 <nelson.alikou-dongmo@etudiant.univ-reims.fr>
2021-01-15 17:27:20 +0100

    Mise à jour du main

--- src/bases/Principal.java
+++ src/bases/Principal.java
@@ -2,6 +2,9 @@ package bases;
 
 public class Principal {
 	public static void main(String[] args) {
-		
+		String prenom = "Nelson";
+		int longueurPrenom = prenom.length();
+		System.out.print(prenom);
+		System.out.print("("+longueurPrenom+" caracteres)");
 	}
 }
