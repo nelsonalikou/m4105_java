f8c3d824cd7990833f997745a8111d0d1596de76
alik0002 <nelson.alikou-dongmo@etudiant.univ-reims.fr>
2021-02-02 10:19:19 +0100

    Fin des tableaux

--- src/bases/Tableaux.java
+++ src/bases/Tableaux.java
@@ -3,6 +3,8 @@
  */
 package bases;
 
+import java.util.Arrays;
+
 /**
  * @author Nelson
  *
@@ -13,7 +15,20 @@ public class Tableaux {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		String[] chaine = {"abc", "aabc", "aaA", "a2", "xyz"};
+		for(int cpt=0; cpt< chaine.length; cpt++){
+			System.out.print(chaine[cpt] + " ");
+		}
+		System.out.println();
+		System.out.println("le nombre de caractères différents est " + variete(chaine[0]));
+		int[] tabNbCharDifferents = histogrammeDesVarietes(chaine);
 
+		for(int cpt=0; cpt< tabNbCharDifferents.length; cpt++){
+			System.out.print(tabNbCharDifferents[cpt] + " ");
+		}
+		System.out.println();
+
+		afficherHistogramme(tabNbCharDifferents);
 	}
 
 	/**
@@ -24,22 +39,102 @@ public class Tableaux {
 	public static int variete(String chaine){
 		chaine =  chaine.toLowerCase();
 		// false initialisation par défaut
-		boolean[] tabCar = new boolean[26];
-		int nbElementsDifferents = 0;
-		//pour chacun des éléments de la chaine
-			for(int i = 0; i < chaine.length(); i++){
-			//je met a case du caractère correspondant à true
-				tabCar[chaine.charAt(i) - 'a'] = true;
-			}
+		boolean[] tabCar = new boolean['z' - 'a' + 1];
+		//int nbElementsDifferents = 0;
 
-			int nbCarDifferents = 0;
-			for(boolean val : tabCar){
-				if(val){
-					nbCarDifferents++;
+		//pour chacun des éléments de la chaine
+			/*for(int i = 0; i < chaine.length(); i++){
+			//je met a case du caractère correspondant à true
+				if(Character.isAlphabetic(chaine.charAt(i))){
+					tabCar[chaine.charAt(i) - 'a'] = true;
+				}
+			}*/
+
+			for(char c : chaine.toLowerCase().toCharArray()){
+				if(Character.isLetter(c)){
+					tabCar[c - 'a'] = true;
 
 				}
 			}
-		return nbCarDifferents;
+
+			int nbCharDifferents = 0;
+			for(boolean val : tabCar){
+				if(val){
+					nbCharDifferents++;
+
+				}
+			}
+		return nbCharDifferents;
+	}
+
+	/**
+	 * Evalue le nombre d'occurance pour chaque caratère et retourne le tableau contenant ces derniers.
+	 * @param chaine chaine à évaluer
+	 * @return tableau contenant l'histogramme des variétes
+	 */
+	public static int[] histogrammeDesVarietes(String[] chaine){
+		//pour stocker les valeurs des variétés trouvées
+		int[] tabNbCharDifferents = new int[chaine.length];
+
+
+		/*
+		// Autre possibilité
+
+		//tabeau de variétés pour chaque chaine de caractères de 'chaine'
+		int[] tableau_des_varietes = Arrays.stream(chaine).mapToInt(e -> variete(e)).toArray();
+		//recherche de la plus grande variété.
+		int max = Arrays.stream(tableau_des_varietes).max().getAsInt();
+		 */
+
+		//calacul des variétés et mémorisation de la plus grande variété
+		int max = Integer.MIN_VALUE;
+			for(int i = 0; i < chaine.length; i++){
+				tabNbCharDifferents[i] = variete(chaine[i]) ;
+				if(tabNbCharDifferents[i] > max){
+					max = tabNbCharDifferents[i];
+				}
+
+			}
+
+			//création du tableau de l'histogramme avec la bonne dimension
+			// Rq : +1 dans la dimension pour gérer la valeur 0
+			// Rq : le tableau est initialisé par défaut à 0
+			int[] res =  new int[max + 1];
+			for(int val : tabNbCharDifferents){
+				res[val]++;
+			}
+
+		return res;
+	}
+
+
+	/**
+	 * Affichage de l'histogramme des valeurs
+	 * @param valeurs
+	 */
+	public static void afficherHistogramme(int valeurs[]) {
+		int hauteur_max = Arrays.stream(valeurs).max().getAsInt();
+		int variete_max = valeurs.length;
+
+		//Pour chaque nombre d'occurences d'une varieté, du plus grand à 0 exclu
+		for(int no_occurence = hauteur_max; no_occurence > 0; no_occurence--){
+			//Pour chaque valeur de variété de 0 à la plus garnde
+			for(int valeur_variete = 0; valeur_variete < variete_max; valeur_variete++){
+				//si la valeur est supérieure ou égale au nombre d'occurences pour cette ligne
+				//  //afficher "XX" //sinon // afficher " "
+				//Afficher " "
+				System.out.print(valeurs[valeur_variete] >= no_occurence ? "XX " : "   ");
+			}
+			//Aller à la ligne
+			System.out.println();
+		}
+
+		//Pour chaque valeur de variete de 0 à la plus grande
+		for(int valeur_variete = 0; valeur_variete < variete_max; valeur_variete++){
+			//Afficher sa valeur
+			System.out.print(String.format("%2d ", valeur_variete));
+		}
+		System.out.println();
 	}
 
 }
--- test/bases/TableauxTest.java
+++ test/bases/TableauxTest.java
@@ -2,7 +2,8 @@ package bases;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class TableauxTest {
@@ -12,7 +13,17 @@ public class TableauxTest {
 		assertEquals(4, Tableaux.variete("abcd"));
 		assertEquals(3, Tableaux.variete("aabc"));
 		assertEquals(1, Tableaux.variete("aaA"));
-		assertEquals(2, Tableaux.variete("a2"));
+		assertEquals(1, Tableaux.variete("a2"));
+	}
+
+	@Test
+	public void histogrammeDesVarietes_test() {
+		String[] chaine = {"abc", "aabc", "aaA", "a2", "xyz"};
+		int[] tabNbCharDifferents = {0, 2, 0, 3};
+		assertEquals(tabNbCharDifferents[0], Tableaux.histogrammeDesVarietes(chaine)[0]);
+		assertEquals(tabNbCharDifferents[1], Tableaux.histogrammeDesVarietes(chaine)[1]);
+		assertEquals(tabNbCharDifferents[2], Tableaux.histogrammeDesVarietes(chaine)[2]);
+		assertEquals(tabNbCharDifferents[3], Tableaux.histogrammeDesVarietes(chaine)[3]);
 	}
 
 }
