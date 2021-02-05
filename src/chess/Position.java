0128becc966b6837d7499b928949dd9b8fb8fe69
alik0002 <nelson.alikou-dongmo@etudiant.univ-reims.fr>
2021-02-05 12:15:30 +0100

    Suite TP, définition de la classe Position et des tests

---
+++ src/chess/Position.java
@@ -0,0 +1,191 @@
+/**
+ *
+ */
+package chess;
+
+/**
+ * @author ALIKOU DONGMO NELSON
+ *
+ * Repr�sente la position d'une pi�ce lors d'une partie d'�checs
+ */
+public class Position {
+
+	/**
+	 * abscisse (comprise entre 0 et 7)
+	 */
+	private int x;
+
+	/**
+	 * ordonn�e (comprise entre 0 et 7)
+	 */
+	private int y;
+
+	/**
+	 * Constructeur de la classe Position cr�e une position � partir des coordonn�es x et y donn�es en param�tre.
+	 * @param x abscisse
+	 * @param y ordonn�e
+	 */
+	public Position(int x, int y){
+		if (x < 0 || y < 0 || x > 7 || y > 7){
+			throw new IllegalArgumentException("Position invalide" + x + " " + y);
+		}
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * Constructeur de la classe Position cr�e une position � partir des coordonn�es x et y donn�es en param�tre.
+	 * @param algebraicNotation cha�ne de 2 caract�res repr�sentant une case de l'�chiquier en notation alg�brique
+	 */
+	public Position(String algebraicNotation){
+
+		char firstCharacter = algebraicNotation.charAt(0);
+		switch(firstCharacter){
+
+			case 'A' : this.x =  0;
+			break;
+			case 'B' : this.x =  1;
+			break;
+			case 'C' : this.x =  2;
+			break;
+			case 'D' : this.x =  3;
+			break;
+			case 'E' : this.x =  4;
+			break;
+			case 'F' : this.x =  5;
+			break;
+			case 'G' : this.x =  6;
+			break;
+			case 'H' : this.x =  7;
+			break;
+
+			default :
+				throw new IllegalArgumentException("Premi�re lettre de la position invalide");
+
+		}
+
+		char secondCharacter = algebraicNotation.charAt(1);
+		if ( Character.getNumericValue(secondCharacter) < 1 || Character.getNumericValue(secondCharacter) > 8 ){
+			throw new IllegalArgumentException("Seconde lettre de la position invalide");
+		}
+		this.y = Character.getNumericValue(algebraicNotation.charAt(1)) - 1;
+	}
+
+	/**
+	 * Accesseur � l'abscisse de la position
+	 * @return l'abscisse de la position
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * Accesseur � l'ordonn�e de la position
+	 * @return l'ordonn�e de la position
+	 */
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * Determine si la position actuelle est sur la m�me ligne qu'une autre pass�e en param�tre.
+	 * @param otherPosition l'autre position
+	 * @return true si laposition courante est sur la m�me ligne que otherPosition
+	 */
+	public boolean isOnSameLineAs(Position otherPosition){
+		return this.y == otherPosition.y;
+	}
+
+	/**
+	 * Determine si la position actuelle est sur la m�me colonne qu'une autre pass�e en param�tre.
+	 * @param otherPosition l'autre position
+	 * @return true si laposition courante est sur la m�me colonne que otherPosition
+	 */
+	public boolean isOnSameColumnAs(Position otherPosition){
+		return this.x == otherPosition.x;
+	}
+
+
+	/**
+	 * Determine si la position actuelle est sur la m�me diagonale qu'une autre pass�e en param�tre.
+	 * @param otherPosition l'autre position
+	 * @return true si laposition courante est sur la m�me diagonale que otherPosition
+	 */
+	public boolean isOnSameDiagonalAs(Position otherPosition){
+		return Math.abs(otherPosition.x - this.x) == Math.abs(otherPosition.y - this.y);
+	}
+
+	/**
+	 * Retourne la distance de Manhattan entre la position courante et une autre position.
+	 * @param otherPosition l'autre position
+	 * @return
+	 */
+	public int getManhattanDistance(Position otherPosition){
+		return Math.abs(otherPosition.x - this.x) + Math.abs(otherPosition.y - this.y);
+	}
+
+
+	public String toString() {
+		   return " " + toAlgebraicNotation();
+	}
+
+	public boolean equals(Object obj) {
+        //Je verifie si mon objet est null auquel cas je retourne false
+		if (obj == null) return false;
+
+        //Je verifie si mon objet est bien de type position, dans le cas contraire je retourne false.
+		if( ! (obj instanceof Position) ) return false;
+
+        Position otherPosition = (Position) obj;
+
+        return (this.x != 0 && this.y != 0) ? (this.x == otherPosition.x && this.y == otherPosition.y) : (otherPosition.x == 0 && otherPosition.y == 0);
+    }
+
+	public int hashCode() {
+        return (x == 0 && y == 0) ? 0 : 1;
+    }
+
+	/**
+	 * Retourne une cha�ne contenant la repr�sentation de la position courante en notation alg�brique
+	 * @return repr�sentation de la position courante en notation alg�brique
+	 */
+	public String toAlgebraicNotation(){
+
+		String algebraicNotation = "";
+
+		switch(this.x){
+
+			case 0 : algebraicNotation +=  'A';
+			break;
+			case 1 : algebraicNotation +=  'B';
+			break;
+			case 2 : algebraicNotation +=  'C';
+			break;
+			case 3 : algebraicNotation +=  'D';
+			break;
+			case 4 : algebraicNotation +=  'E';
+			break;
+			case 5 : algebraicNotation +=  'F';
+			break;
+			case 6 : algebraicNotation +=  'G';
+			break;
+			case 7 : algebraicNotation +=  'H';
+			break;
+		}
+
+		//J'ajoute à la chaine la valeur de l'ordonnée augmentée de 1 et convertie en base décimale.
+		algebraicNotation += Character.forDigit(this.y + 1 ,10);
+
+		return algebraicNotation;
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ test/chess/PositionTest.java
@@ -0,0 +1,129 @@
+package chess;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PositionTest {
+
+	private Position[] positions = new Position[6] ;
+
+	@Before
+	public void setUp() throws IllegalArgumentException {
+
+		positions[0] = new Position("E5");
+		positions[1] = new Position(4,5);
+		positions[2] = new Position(3,4);
+		positions[3] = new Position(5,6);
+		positions[4] = new Position(4,4);
+		positions[5] = new Position(4,4);
+
+	}
+
+	@Test
+	public void isOnSameLineAs_verification() {
+		try {
+			assertTrue(positions[0].isOnSameLineAs(positions[2]));
+			//fail("IllegalArgumentException n'a pas �t� lanc�e");
+			}
+		catch (IllegalArgumentException e) {}
+	}
+
+	@Test
+	public void isOnSameColumnAs_verification() {
+		try {
+			assertTrue(positions[0].isOnSameColumnAs(positions[1]));
+
+			}
+		catch (IllegalArgumentException e) {}
+	}
+
+	@Test
+	public void isOnSameDiagonalAs_verification() {
+		try {
+			assertTrue(positions[2].isOnSameDiagonalAs(positions[3]));
+			}
+		catch (IllegalArgumentException e) {}
+	}
+
+	@Test
+	public void getManhattanDistance_verification() {
+		try {
+			assertEquals(0, positions[0].getManhattanDistance(positions[4]));
+			}
+		catch (IllegalArgumentException e) {}
+	}
+
+	@Test
+	public void toAlgebraicNotation_conversion() {
+		try {
+			assertEquals("E5", positions[4].toAlgebraicNotation());
+			}
+		catch (IllegalArgumentException e) {}
+	}
+
+
+	@Test
+    public void should_be_equals() throws Exception {
+
+        /* Reflexive */
+		assertTrue(positions[0].equals(positions[0]));
+		assertTrue(positions[4].equals(positions[4]));
+		assertTrue(positions[5].equals(positions[5]));
+
+        /* Symmetric */
+		assertTrue(positions[0].equals(positions[4]));
+		assertTrue(positions[4].equals(positions[0]));
+
+        /* Transitive */
+		assertTrue(positions[0].equals(positions[4]));
+		assertTrue(positions[4].equals(positions[5]));
+		assertTrue(positions[0].equals(positions[5]));
+
+        assertFalse(positions[0].equals(null));
+        assertFalse(positions[4].equals(null));
+        assertFalse(positions[5].equals(null));
+
+    }
+
+
+	@Test
+    public void testHashSet() throws Exception {
+
+        final Set<Position> Positions = new HashSet<>();
+        Positions.add(positions[0]);
+
+        assertEquals(Positions.iterator().next(),positions[4]);
+        assertTrue(Positions.contains(positions[4]));
+    }
+
+    @Test
+    public void testHashMap() throws Exception {
+
+        final Map<Integer, Position> map = new HashMap<>();
+        map.put(2, positions[0]);
+
+        assertEquals(1,map.size());
+        //assertEquals(positions[4],map.keySet().iterator().next());
+        assertEquals(positions[4],map.values().iterator().next());
+        assertTrue(map.containsValue(positions[4]));
+    }
+
+    @Test
+    public void should_not_contain_doubles() throws Exception {
+
+        final Set<Position> Positions = new HashSet<>();
+        Positions.add(positions[0]);
+        Positions.add(positions[4]);
+
+        assertTrue(positions[0].equals(positions[4]));
+        assertEquals(1,Positions.size());
+    }
+
+}
