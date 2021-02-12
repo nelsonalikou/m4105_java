21f62547e5bae200c589776a3065fabf9a862022
alik0002 <nelson.alikou-dongmo@etudiant.univ-reims.fr>
2021-02-12 17:25:35 +0100

    création classes Pair et TestHashSet.

---
+++ src/collection/Classes.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+package collection;
+
+/**
+ * @author ALIKOU DONGMO NELSON
+ *
+ */
+public class Classes {
+
+	public static void main(String [] args) {
+		Pair<String,Integer> pair = new Pair<>("chaine", 12);
+		System.out.println(pair.getFirst());
+		System.out.println(pair.getSecond());
+	}
+
+}
---
+++ src/collection/Pair.java
@@ -0,0 +1,28 @@
+/**
+ *
+ */
+package collection;
+
+/**
+ * @author ALIKOU DONGMO NELSON
+ *
+ */
+public class Pair<U,V> {
+
+	private U first;
+
+	private V second;
+
+	public Pair(U first, V second) {
+		this.first = first ;
+		this.second = second;
+	}
+
+	public U getFirst() {
+		return this.first;
+	}
+
+	public V getSecond() {
+		return this.second;
+	}
+}
